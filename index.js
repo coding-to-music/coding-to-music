f5fdd7e4ba6d8539605a4164d7d50012520b333a
coding-to-music <connors.tom@gmail.com>
2020-12-30 17:57:03 -0500

    close to working

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+require('dotenv').config();
 const Mustache = require('mustache');
+const fetch = require('node-fetch');
 const fs = require('fs');
+const puppeteerService = require('./services/puppeteer.service');
+
 const MUSTACHE_MAIN_DIR = './main.mustache';
 /**
   * DATA is the object that contains all
   * the data to be provided to Mustache
   * Notice the "name" and "date" property.
 */
+console.log("In index.js......");
+
 let DATA = {
   name: 'Tom',
   refresh_date: new Date().toLocaleDateString('en-US', {
@@ -23,11 +29,76 @@ let DATA = {
   * B - We ask Mustache to render our file with the data
   * C - We create a README.md file with the generated output
   */
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+
+ async function setWeatherInformation() {
+  console.log("In setWeatherInformation - just arrived ......");
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  )
+    .then(r => r.json())
+    .then(r => {
+      console.log("In setWeatherInformation - then section BEFORE ......");
+      DATA.city_temperature = Math.round(r.main.temp);
+      DATA.city_weather = r.weather[0].description;
+      DATA.city_weather_icon = r.weather[0].icon;
+      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'America/New_York',
+      });
+      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'America/New_York',
+      });
+      console.log("In setWeatherInformation - then section AFTER ......");
+      console.log(JSON.stringify(DATA));
+
+    });
+}
+
+async function setInstagramPosts() {
+  console.log("In setInstagramPosts - ......");
+  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('visitstockholm', 3);
+  DATA.img1 = instagramImages[0];
+  DATA.img2 = instagramImages[1];
+  DATA.img3 = instagramImages[2];
+}
+
+async function generateReadMe() {
+  console.log("In setInstagramPosts - ......");
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
   });
 }
-generateReadMe();
+
+async function action() {
+  /**
+   * Fetch Weather
+   */
+  console.log("In action - about to await setWeatherInformation ......");
+  await setWeatherInformation();
+
+  /**
+   * Get pictures
+   */
+  console.log("In action - about to await setInstagramPosts ......");
+  await setInstagramPosts();
+
+  /**
+   * Generate README
+   */
+  console.log("In action - about to await generateReadMe ......");
+  await generateReadMe();
+
+  /**
+   * Fermeture de la boutique 👋
+   */
+  console.log("In action - about to await puppeteerService.close ......");
+  await puppeteerService.close();
+}
+
+console.log("In Index - about to call action ......");
+action();
--- services/puppeteer.service.js
+++ services/puppeteer.service.js
@@ -4,27 +4,33 @@ class PuppeteerService {
   page;
 
   async init() {
+    console.log("In puppeteerService - Opening the browser......");
     this.browser = await puppeteer.launch({
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-infobars',
-        '--window-position=0,0',
-        '--ignore-certifcate-errors',
-        '--ignore-certifcate-errors-spki-list',
-        '--incognito',
-        '--proxy-server=http=194.67.37.90:3128',
-        // '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"', //
-      ],
-      // headless: false,
-    });
-  }
+      headless: true,
+      args: ["--disable-setuid-sandbox"],
+      'ignoreHTTPSErrors': true
+  });
 
+  console.log("In puppeteerService - after the async init......");
+
+}
+
+  // '--no-sandbox',
+  // '--disable-setuid-sandbox',
+  // '--disable-infobars',
+  // '--window-position=0,0',
+  // '--ignore-certifcate-errors',
+  // '--ignore-certifcate-errors-spki-list',
+  // '--incognito',
+  // '--proxy-server=http=194.67.37.90:3128',
+  // headless: false,
+  // '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"', //
   /**
    *
    * @param {string} url
    */
   async goToPage(url) {
+    console.log("In goToPage - ......");
     if (!this.browser) {
       await this.init();
     }
@@ -40,6 +46,7 @@ class PuppeteerService {
   }
 
   async close() {
+    console.log("In close - ......");
     await this.page.close();
     await this.browser.close();
   }
@@ -50,6 +57,7 @@ class PuppeteerService {
    * @param {number} n Qty of image to fetch
    */
   async getLatestInstagramPostsFromAccount(acc, n) {
+    console.log(`In getLatestInstagramPostsFromAccount - ...... ${acc}`);
     const page = `https://www.picuki.com/profile/${acc}`;
     await this.goToPage(page);
 
